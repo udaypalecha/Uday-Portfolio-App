9914c3ff4ad8290e11ef0ae1aef293679ea77d4d
Uday Palecha <99619714+udaypalecha@users.noreply.github.com>
2023-10-25 03:40:27 +0530

    Comment grey color of 1st Skills section

--- lib/widget/skillsContainer.dart
+++ lib/widget/skillsContainer.dart
@@ -2,23 +2,23 @@ import 'package:flutter/material.dart';
 
 
 class SkillsContainer extends StatelessWidget {
-
   final String SkillsTitle;
-  const SkillsContainer({super.key , required this.SkillsTitle});
+  const SkillsContainer({super.key, required this.SkillsTitle});
 
   @override
   Widget build(BuildContext context) {
-    return   Container(
+    return Container(
       padding:const EdgeInsets.symmetric(horizontal: 12 , vertical: 10),
       decoration: BoxDecoration(
-          color: Colors.grey,
-          borderRadius: BorderRadius.circular(20)
+        // color: Colors.grey,
+        borderRadius: BorderRadius.circular(20)
       ),
-      child: Text(SkillsTitle,
+      child: Text(
+        SkillsTitle,
         style:const TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-            fontFamily: "Lexend"
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+          fontFamily: "Lexend"
         ),
       ),
     );

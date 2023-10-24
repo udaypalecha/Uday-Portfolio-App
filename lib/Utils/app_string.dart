c959bee7feca19f990b9e712a32d79c1a146a99f
Uday Palecha <99619714+udaypalecha@users.noreply.github.com>
2023-10-25 03:39:51 +0530

    Update and add AppStrings (Heading)

--- lib/Utils/app_string.dart
+++ lib/Utils/app_string.dart
@@ -1,9 +1,9 @@
-
 class AppString{
   AppString._();
 
-  static const String   ProfileAsset = "asset/r j.jpg";
-  static const String   BioScreen =  " 🚀  BIO";
-  static const String  HobbiesScreen = " 🥳  Hobbies";
-  static const String  ContactScreen = "💥 Contact";
+  static const String ProfileAsset = "asset/Uday_Palecha.jpg";
+  static const String BioScreen =  "🎓 Professional Summary";
+  static const String HobbiesScreen = "🥳 Hobbies";
+  static const String ContactScreen = "📞 Contact";
+  static const String ProjectsScreen = "📚 Projects";
 }
